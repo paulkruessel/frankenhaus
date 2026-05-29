3d7b603391482f7fa3fa8165750b651cd869cd4f
paulkruessel <kruesselpaul@gmail.com>
2026-05-29 18:33:54 +0200

    Initial Commit

---
+++ frankenhaus-backend/src/main/java/dev/kruessel/frankenhaus_backend/FrankenhausBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.kruessel.frankenhaus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankenhausBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankenhausBackendApplication.class, args);
+	}
+
+}
---
+++ frankenhaus-backend/src/test/java/dev/kruessel/frankenhaus_backend/FrankenhausBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kruessel.frankenhaus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankenhausBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
