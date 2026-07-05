79bca0ec30f5158ff818ebd36c67804614c4f23c
Lord-Juice <kruesselpaul@gmail.com>
2026-07-05 16:25:36 +0200

    Reinstalled angular

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Mainpage } from "./mainpage/mainpage";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Mainpage],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Frankenhaus');
+  protected readonly title = signal('client');
 }
