b62a5801171e02975721da6ba17c1e8ed9c38841
paulkruessel <kruesselpaul@gmail.com>
2026-09-16 21:05:07 +0200

    feat(corps): add EntityGraph to findByUserId for optimized data retrieval

--- adh/src/main/java/de/franconia/tuebingen/adh/corps/CorpsMembershipRepository.java
+++ adh/src/main/java/de/franconia/tuebingen/adh/corps/CorpsMembershipRepository.java
@@ -4,12 +4,18 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CorpsMembershipRepository
         extends JpaRepository<CorpsMembership, UUID> {
 
-    List<CorpsMembership> findByUserId(UUID userId);
+    @EntityGraph(attributePaths = {
+            "corps"
+    })
+    List<CorpsMembership> findByUserId(
+            UUID userId
+    );
 
     Optional<CorpsMembership> findByIdAndUserId(
             UUID id,
--- adh/src/main/java/de/franconia/tuebingen/adh/corps/CorpsRepository.java
+++ adh/src/main/java/de/franconia/tuebingen/adh/corps/CorpsRepository.java
@@ -1,11 +1,9 @@
 package de.franconia.tuebingen.adh.corps;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.EntityGraph;
 
 public interface CorpsRepository
         extends JpaRepository<Corps, UUID> {
@@ -13,10 +11,4 @@ public interface CorpsRepository
     Optional<Corps> findByName(String name);
 
     boolean existsByName(String name);
-
-    @EntityGraph(attributePaths = {
-            "corps"
-    })
-    List<CorpsMembership> findByUserId(
-            UUID userId);
 }
